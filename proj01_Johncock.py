308263c81e1cb7f912939f1c035cca6ee1f8b7ce
Leemon Rafael Johncock <ljohncock01@gmail.com>
2025-09-03 18:08:17 -0400

    Add files via upload

---
+++ proj01_Johncock.py
@@ -0,0 +1,22 @@
+#converting chains to other functions
+print("Welcome to using the NYC Subway distance converter!")
+print(" ")
+chains = float( input ("please enter distance in chains"))
+
+feet = chains * 66
+meters = feet * 0.3048
+miles = feet / 5280
+kilometers = meters / 1000
+time = (miles / 3.1) * 60
+feet = round(feet,2)
+meters = round(meters, 2)
+miles = round(miles, 3)
+kilometers = round(kilometers, 3)
+time = round(time, 1)
+print ("distance:", chains, "chains")
+print ("meters:", meters)
+print ("feet:", feet)
+print ("miles:", miles)
+print ("kilometers:", kilometers)
+print ("minitues:", time)
+print ("thank you for using the converter. Goodbye!")
